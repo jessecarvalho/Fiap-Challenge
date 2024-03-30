254da78434a23e85874d99ec38c7a4768ca4d33d
jessecarvalho <jesseelias80@gmail.com>
2024-03-30 17:57:14 -0300

    [feat] Created validator for course

---
+++ Application/Validators/CourseRequestDtoValidator.cs
@@ -0,0 +1,25 @@
+using Application.Dto;
+using Core.Enums;
+using FluentValidation;
+
+namespace Application.Validators;
+
+public class CourseRequestDtoValidator : AbstractValidator<CourseRequestDto>
+{
+
+    public CourseRequestDtoValidator()
+    {
+        RuleFor(x => x.Title)
+            .NotEmpty().WithMessage("Title is required")
+            .Length(0, 255).WithMessage("Title must not exceed 255 characters");
+
+        RuleFor(x => x.Description)
+            .NotEmpty().WithMessage("Description is required")
+            .Length(0, 500).WithMessage("Description must not exceed 500 characters");
+
+        RuleFor(x => x.Year)
+            .IsInEnum().WithMessage("Year is not valid");
+
+    }
+    
+}
