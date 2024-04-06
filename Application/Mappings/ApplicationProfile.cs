abaafe22bc46d09e045904d0e2f3c9f6ef1adcab
jessecarvalho <jesseelias80@gmail.com>
2024-04-06 11:11:53 -0300

    [refactor] Removed unnecessary lines

--- Application/Mappings/ApplicationProfile.cs
+++ Application/Mappings/ApplicationProfile.cs
@@ -12,9 +12,5 @@ public class ApplicationProfile : Profile
         CreateMap<Course, CourseResponseDto>();
         CreateMap<StudentRequestDto, Student>();
         CreateMap<Student, StudentResponseDto>();
-        CreateMap<IEnumerable<StudentResponseDto>, IEnumerable<Student>>();
-        CreateMap<IEnumerable<Student>, IEnumerable<StudentRequestDto>>();
-        CreateMap<IEnumerable<CourseRequestDto>, IEnumerable<Course>>();
-        CreateMap<IEnumerable<Course>, CourseResponseDto>();
     }
 }
