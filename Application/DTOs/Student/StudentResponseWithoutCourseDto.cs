78952aacc996b679a81204c1a551192c693e56f8
jessecarvalho <jesseelias80@gmail.com>
2024-04-12 20:38:03 -0300

    [feat] Created DTO for student without courses

---
+++ Application/DTOs/Student/StudentResponseWithoutCourseDto.cs
@@ -0,0 +1,8 @@
+namespace Application.DTOs;
+
+public class StudentResponseWithoutCourseDto
+{
+    public string Name { get; set; }
+    public string Email { get; set; }
+    public string UserName { get; set; }
+}
